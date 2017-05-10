0cf13a021d40b3934e100b4e9bda251848904f72
CPedersen3245 <campbelljip@gmail.com>
2017-05-10 16:39:30 +0800

    File uploader now shows list, simulates upload and shows icons in list

--- File_Upload_Frontend/assets/js/functions.js
+++ File_Upload_Frontend/assets/js/functions.js
@@ -3,20 +3,27 @@
 /*********************************/
 var browseButton = document.getElementById('file-browse');
 var fileInput = document.getElementById('file-input');
-var uploadWidgetOuter= document.getElementById('upload-container');
+var uploadWidgetInner = document.getElementById('drag-target')
+var errorMessageSpan = document.getElementById('error-text')
 
 /*********************************/
 /*      DOM Element Events       */
 /*********************************/
+//Button for selecting files
 browseButton.onclick = function() {
   fileInput.click();
 };
+
+//Disable listener for dragging files onto all of the window
+window.addEventListener("dragover",function(e){
+  e.preventDefault();
+},false);
 
 /*********************************/
 /*      GENERAL USE METHODS      */
 /*********************************/
 /*
- * Name: containsObject
+ * Name: containsFile
  *
  * Purpose: Checks if a file is in a list of files
  *
@@ -63,23 +70,83 @@ app.run(function($rootScope) {
   /*  SCOPE INITIALIZATION  */
   /**************************/
   $rootScope.filelist = [];
+  $rootScope.fileState = [];
+  $rootScope.draggedover = false;
+  $rootScope.filledFilelist = false;
+  $rootScope.errorMessage = false;
+  $rootScope.uploading = false;
 
   /*
    * Name: removeFile
    *
    * Purpose: Removes the specified file from the scope's file list
    *
-   * Params: file, the file object to remove
+   * Params: index, the index of the file to remove
    *
    * Return: N/A
    *
    * Notes: N/A
    */
-  $rootScope.removeFile = function(file) {
-    var index = $rootScope.filelist.indexOf(file);
-    $rootScope.filelist.splice(index, 1);
-    console.log($rootScope.filelist)
+  $rootScope.removeFile = function(index) {
+    if(index > -1) {
+      $rootScope.filelist.splice(index, 1);
+      $rootScope.fileState.splice(index, 1);
+      if($rootScope.filelist.length == 0){
+        $rootScope.filledFilelist = false;
+      }
+    }
   };
+
+  /*
+   * Name: addFiles
+   *
+   * Purpose: Adds a set of files to the scope, provided they're not already present
+   *
+   * Params: inFiles, a file list object
+   *
+   * Return: N/A
+   *
+   * Notes: N/A
+   */
+  function addFiles(inFiles) {
+    for(var i = 0; i < inFiles.length; i++) {
+      if(inFiles[i].type != "application/pdf") {
+        showErrorMessage("Non-PDF files entered, please only input PDF files.")
+      }
+      else if(!containsFile(inFiles[i], $rootScope.filelist)) {
+        $rootScope.filelist.push(inFiles[i]);
+        $rootScope.fileState.push('selecting');
+      }
+    }
+    if($rootScope.filelist.length > 0){
+      $rootScope.filledFilelist = true;
+    }
+    else {
+      $rootScope.filledFilelist = false;
+    }
+    $rootScope.$apply();
+    fileInput.value = "";
+  }
+
+  /*
+   * Name: showErrorMessage
+   *
+   * Purpose: Shows a brief error message on screen
+   *
+   * Params: inputMessage, the string to be shown
+   *
+   * Return: none
+   *
+   * Notes: N/A
+   */
+  function showErrorMessage(inputMessage) {
+    $rootScope.errorText = inputMessage
+    $rootScope.errorMessage = true;
+    setTimeout(function() {
+      $rootScope.errorMessage = false;
+      $rootScope.$apply();
+    }, 4000);
+  }
 
   /*
    * Name: fileInput.onChange
@@ -94,14 +161,74 @@ app.run(function($rootScope) {
    * Notes: N/A
    */
   fileInput.onchange = function() {
-    for(var i = 0; i < this.files.length; i++) {
-      if(!containsFile(this.files[i], $rootScope.filelist)) {
-        $rootScope.filelist.push(this.files[i]);
-      }
-    }
+    addFiles(this.files)
+  };
+
+  /*
+   * Name: uploadWidgetInner.ondrop
+   *
+   * Purpose: Handler for file input via drag and drop
+   *
+   * Params: e, The event object JS generates
+   *
+   * Return: none
+   *
+   * Notes: N/A
+   */
+   $rootScope.uploadFiles = function() {
+     if($rootScope.filelist.length > 0) {
+       $rootScope.uploading = true;
+       for(var i = 0; i < $rootScope.filelist.length; i++) {
+         $rootScope.fileState[i] = "uploading";
+         setupAjaxRequest(i)
+       }
+     }
+     else {
+       showErrorMessage("Please input files before uploading.")
+     }
+   }
+
+   function setupAjaxRequest(fileIndex) {
+     setTimeout(function(){
+       if(1) {//Math.random() >= 0.5) {
+         $rootScope.fileState[fileIndex] = "success";
+       }
+       else {
+         $rootScope.fileState[fileIndex] = "failure";
+       }
+       if($rootScope.fileState.indexOf("uploading") < 0) {
+         $rootScope.uploading = false;
+       }
+       $rootScope.$apply();
+     }, Math.floor(Math.random() * 6000) + 2000);
+   }
+
+  /*
+   * Name: uploadWidgetInner.ondrop
+   *
+   * Purpose: Handler for file input via drag and drop
+   *
+   * Params: e, The event object JS generates
+   *
+   * Return: none
+   *
+   * Notes: N/A
+   */
+  uploadWidgetInner.ondrop = function(e) {
+    e.preventDefault();
+    addFiles(e.dataTransfer.files)
+    $rootScope.draggedover = false;
     $rootScope.$apply();
-    console.log($rootScope.filelist)
-    fileInput.value = "";
+  };
+
+  uploadWidgetInner.ondragover = function() {
+    $rootScope.draggedover = true;
+    $rootScope.$apply();
+  };
+
+  uploadWidgetInner.ondragleave = function() {
+    $rootScope.draggedover = false;
+    $rootScope.$apply();
   };
 
   //Finally, apply the scope and let the magic happen!
