26300067f56ee39d211ecfd68a452f6998e9859b
Seven <seven.albany.bi@gmail.com>
2017-05-12 23:14:39 +0800

    bug fix form template and plan

--- core_app/views.py
+++ core_app/views.py
@@ -2,7 +2,6 @@ from .models import Student, StudentUnit, CourseTemplate
 from django.http import JsonResponse
 import json
 
-
 ######################################################################################################
 # For retrieving the student course progress information, 1st get the student id
 # from request with json object, and retrieving the data from data model and forming
@@ -13,14 +12,14 @@ def course_progress(request):
         try:
             received_data = json.loads(request.body.decode('utf-8'))
         except Student.DoesNotExist:
-            raise error_message = 'errors json object'
+            raise RuntimeError('errors: can not parse json object') from error
         try:
             student_id = received_data.get['id']
             student = Student.objects.get(pk=student_id)
-            all_template = CourseTemplate.objects.all().filter(CourseID=student.CourseID).values()
-            all_plan = StudentUnit.objects.all().filter(StudentID=student.StudentID).values()
+            all_template = CourseTemplate.objects.all().filter(CourseID=student.CourseID)
+            all_plan = StudentUnit.objects.all().filter(StudentID=student.StudentID)
         except Student.DoesNotExist:
-            raise error_message = 'wrong student ID'
+            raise RuntimeError('errors: wrong student ID input') from error
     else:
         raise Http404()
 
@@ -43,29 +42,29 @@ def return_resp(templates, plans):
 
 ######################################################################################################
 # Finding each data as required and save and construct with the pre-defined
-# json data, for both templates and plans
+# json data, for both templates and plans, i.e.# template = {'1':{'2':{'16102183':'SE200', 'credits',:'25'}}}
 ######################################################################################################
 def form_templates(all_template):
-    templates = {}
-    years = {}
-    semesters = {}
+    template = {}
     for temp in all_template:
-        templates[temp.Year] = temp.Semester
-        years[temp.Semester] = temp.UnitID
         single_unit = Unit.object.get(pk=temp.UnitID)
-        semesters[temp.UnitID] = {'name':single_unit.Name, 'credits':single_unit.Credits}
+        unit = {'name':single_unit.Name, 'credits':single_unit.Credits}
+        semester[unit.UnitID] = unit
+        semester_info = temp.Semester
+        year_info = temp.Year
+        template.update({year_info:{semester_info:semester}})
 
-    return templates
+    return template
 
 def form_plans(all_plans):
-    plans = {}
-    years = {}
-    semesters = {}
-    for plan in all_plans:
-        templates[plan.Year] = plan.Semester
-        years[plan.Semester] = plan.UnitID
-        single_unit = Unit.object.get(pk=plan.UnitID)
-        semesters[plan.UnitID] = {'name':single_unit.Name, 'credits':single_unit.Credits, 'attempts':plan.Attempts}
+    plan = {}
+    for pl in all_plans:
+        single_unit = Unit.object.get(pk=pl.UnitID)
+        unit = {'name':single_unit.Name, 'credits':single_unit.Credits, 'attempts':pl.Attemps}
+        semester[unit.UnitID] = unit
+        semester_info = pl.Semester
+        year_info = pl.Year
+		plan.update({year_info:{semester_info:semester}})
 
-    return plans
+    return plan
 ######################################################################################################
