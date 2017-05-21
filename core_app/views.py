1ef6724d69937f98b133e223d829753020dbe683
Seven <seven.albany.bi@gmail.com>
2017-05-21 16:02:20 +0800

    initial function and save unfinished work

--- core_app/views.py
+++ core_app/views.py
@@ -2,69 +2,72 @@ from .models import Student, StudentUnit, CourseTemplate
 from django.http import JsonResponse
 import json
 
-######################################################################################################
-# For retrieving the student course progress information, 1st get the student id
-# from request with json object, and retrieving the data from data model and forming
-# the new json object return back to the Front-end
-######################################################################################################
-def course_progress(request):
-    if request.is_ajax():
+
+student_id = -1
+# If all good then add units to completedUnits array waiting to save
+valid_enrol_units = []
+def enrol_plan_validity(request):
+    if student_id is not -1 AND request.is_ajax():
         try:
-            received_data = json.loads(request.body.decode('utf-8'))
+            received_plan_json = json.loads(request.body.decode('utf-8'))
         except Student.DoesNotExist:
             raise RuntimeError('errors: can not parse json object') from error
         try:
-            student_id = received_data.get['id']
-            student = Student.objects.get(pk=student_id)
-            all_template = CourseTemplate.objects.all().filter(CourseID=student.CourseID)
-            all_plan = StudentUnit.objects.all().filter(StudentID=student.StudentID)
+            boolean_respond = validity_query(received_plan_json)
         except Student.DoesNotExist:
-            raise RuntimeError('errors: wrong student ID input') from error
+            raise RuntimeError('') from error
     else:
         raise Http404()
 
-    templates = form_templates(all_template)
-    plans = form_plans(all_plan)
-    resp = return_couseTemp(templates, plans)
 
-    return JsonResponse(resp)
+def validity_query(received_plan_json):
+    for key, value in received_plan_json.items():
+        year = key
+        sem_value = value
+        for key, value in sem_value.items():
+            sem = Key
+            unit_value = value
+            credits_this_semester = 0
+            for key, value in unit_value.items():
+                unit_id = Key
+                unit_info = Value
 
-######################################################################################################
-# Form the json object
-######################################################################################################
-def return_resp(templates, plans):
-    resp = {
-        'template': templates
-        'plan': plans
-    }
+                if unit_id is not 'ELECTIVE'
+                    completed_unit = StudentUnits.objects.get(StudentID=student_id AND UnitID=unit_id)
+                    # this is course temp object
+                    if not completed_unit
+                        # check the unit if belongs to this course
+                        option = CourseTemplateOptions.objects.get(UnitID=unit_id)
+                        unit_course_id = CourseTemplate.objects.get(Option=option)
+                        student_course_id = Courses.objects.get(StudentID=student_id)
+                        if student_course_id is not unit_course_id
+                            # check equalence if this unit is a equivalence unit
+                            # if ..
+                                # return False
 
-    return resp
+                        # check prerequisites true or false true go next
+                        option = CourseTemplateOptions.objects.get(UnitID=unit_id)
+                        prerequisite_units = Prerequisite.objects.all().filter(Option=option)
+                        for unit in prerequisite_units:
+                            a_unit = StudentUnits.objects.get(UnitID=unit.UnitID)
+                            if not StudentUnits.objects.get(UnitID=unit.UnitID)
+                                return False
+                            elif a_unit.PrerequisiteAchieved is False or a_unit.Status is 3 or a_unit.Status is 1 or a_unit.Year is not year or a_unit.Semester is not sem
+                                return False
+                        # add credits
+                    else
+                        # here will need to generate an error message to denote what problems are with enrollment (elif)
+                        if completed_unit.Status is 2 or completed_unit.Year is not year or completed_unit.Semester is not sem
+                            del valid_enrol_units[:]
+                            return False
+                        else
+                            credits_this_semester += unit_info['credits']
+                            valid_enrol_units.append(unit)
+                # unit is elective
+                else
+                    credits_this_semester += unit_info['credits']
 
-######################################################################################################
-# Finding each data as required and save and construct with the pre-defined
-# json data, for both templates and plans, i.e.# template = {'1':{'2':{'16102183':'SE200', 'credits',:'25'}}}
-######################################################################################################
-def form_templates(all_template):
-    template = {}
-    for temp in all_template:
-        single_unit = Unit.object.get(pk=temp.UnitID)
-        unit = {'name':single_unit.Name, 'credits':single_unit.Credits}
-        semester[unit.UnitID] = unit
-        semester_info = temp.Semester
-        year_info = temp.Year
-        template.update({year_info:{semester_info:semester}})
-
-    return template
-
-def form_plans(all_plans):
-    plan = {}
-    for pl in all_plans:
-        single_unit = Unit.object.get(pk=pl.UnitID)
-        unit = {'name':single_unit.Name, 'credits':single_unit.Credits, 'attempts':pl.Attemps}
-        semester[unit.UnitID] = unit
-        semester_info = pl.Semester
-        year_info = pl.Year
-		plan.update({year_info:{semester_info:semester}})
-
-    return plan
-######################################################################################################
+            # Credit amount  per semester musn't exceed 100
+            if credits_this_semester > 100
+                del valid_enrol_units[:]
+                return False
