84b1d20be641119ef83faea36d0d08b294a5268c
CPedersen3245 <campbelljip@gmail.com>
2017-05-22 17:11:24 +0800

    Fixed max height of student select table and border thickness

--- Planner/static/core_app/js/student_select.js
+++ Planner/static/core_app/js/student_select.js
@@ -13,13 +13,14 @@ app.run(function($rootScope) {
   $rootScope.selectStudent = function(id) {
     $rootScope.selectedStudentID = id;
   };
-  
+
   //Testing input only
   var json =  { '16171921': 'Campbell Pedersen',
                 '16365481': 'Chung-Yen Lu',
                 '16102183': 'Chen Bi',
                 '17080170': 'Yoakim Persson',
-                '17898755': 'Thien Quang Trinh' };
+                '17898755': 'Thien Quang Trinh'
+              };
 
   //Append student JSON to array
   angular.forEach(json, function (value, key) {
