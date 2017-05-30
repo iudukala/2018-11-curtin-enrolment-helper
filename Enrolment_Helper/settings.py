cd414d7d90f78492e1041062e382525119deb22b
Seven <seven.albany.bi@gmail.com>
2017-05-31 04:19:19 +0800

    ver-02 apply change of json new structure

--- Enrolment_Helper/settings.py
+++ Enrolment_Helper/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
    # 'core_app.apps.CoreAppConfig',
-    'core_app',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +83,14 @@ DATABASES = {
     # }
 
     # Utilising the MySQL database
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'Enrolment_Helper',
+    #    'USER': 'enrolment_helperuser',
+    #    'PASSWORD': 'user',
+    #    'HOST': 'localhost',
+    #    'PORT': '',
+    #}
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_enrolment_helper',
